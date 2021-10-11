d093440a0c77eb314531acc40f78f046f09f18af
Kyya Wang <kyya.wang@gmail.com>
2021-10-11 13:58:58 +0800

    Support more keywords: group, aggregateWindow, yield

--- src/json2flux.test.ts
+++ src/json2flux.test.ts
@@ -1,11 +1,23 @@
+import fluxQueryComplex from './flux-query-complex.json';
 import fluxQuery from './flux-query.json';
 import json2flux, { FluxJsonQuery } from './json2flux';
 
-test('It should return correct flux sql', () => {
+test('It should return correct flux query', () => {
   expect(json2flux(fluxQuery as FluxJsonQuery)).toEqual(`from(bucket: "mainflux_1h")
   |> range(start: -7d)
   |> filter(fn: (r) => r["building"] == "mock-building")
   |> filter(fn: (r) => r["_measurement"] == "CO2")
   |> filter(fn: (r) => r["device"] == "Air-Quality-Sensor")
   |> filter(fn: (r) => r["_field"] == "value")`);
+});
+
+test('It should return correct complex flux query', () => {
+  expect(json2flux(fluxQueryComplex as FluxJsonQuery)).toEqual(`from(bucket: "mainflux")
+  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+  |> filter(fn: (r) => r["_measurement"] == "ActiveEnergyTotal")
+  |> filter(fn: (r) => r["_field"] == "value")
+  |> filter(fn: (r) => r["level"] == "B1" or r["level"] == "B2" or r["level"] == "L10")
+  |> group(columns: ["level"])
+  |> aggregateWindow(every: 2m, fn: mean, createEmpty: false)
+  |> yield(name: "mean")`);
 });
--- src/json2flux.ts
+++ src/json2flux.ts
@@ -1,15 +1,29 @@
-import { map } from 'lodash';
+import { isArray, join, map } from 'lodash';
 
 export interface FilterQuery {
   key: string;
-  value: string;
+  value: string | string[];
   operator: 'eq' | 'neq' | 'gt' | 'lt' | 'ge' | 'le';
 }
 
 export interface FluxJsonQuery {
   bucket: string;
-  range: string;
+  range: RangeOptions | string;
   filters: FilterQuery[];
+  group?: string[];
+  aggregateWindow?: AggregateWindowOptions;
+  yield?: string;
+}
+
+export interface AggregateWindowOptions {
+  every: string;
+  fn: 'mean' | 'sum';
+  createEmpty: boolean;
+}
+
+export interface RangeOptions {
+  start: string;
+  stop: string;
 }
 
 export function from(bucket: string): string {
@@ -17,6 +31,13 @@ export function from(bucket: string): string {
 }
 
 export function filter({ key, value, operator }: FilterQuery): string {
+  if (isArray(value)) {
+    const conditions: string = join(
+      map(value, (_value) => `r["${key}"] ${op(operator)} "${_value}"`),
+      ' or ',
+    );
+    return `filter(fn: (r) => ${conditions})`;
+  }
   return `filter(fn: (r) => r["${key}"] ${op(operator)} "${value}")`;
 }
 
@@ -39,15 +60,47 @@ export function op(value: string): string {
   }
 }
 
-export function range(start: string): string {
-  return `range(start: ${start})`;
+export function range(options: RangeOptions | string): string {
+  if (typeof options === 'object') {
+    return `range(start: ${options.start}, stop: ${options.stop})`;
+  }
+  return `range(start: ${options})`;
+}
+
+function group(values: string[]) {
+  return `group(columns: ${JSON.stringify(values)})`;
+}
+
+function aggregateWindow(options: AggregateWindowOptions) {
+  return `aggregateWindow(every: ${options.every}, fn: ${options.fn}, createEmpty: ${JSON.stringify(
+    options.createEmpty,
+  )})`;
+}
+
+function fluxYield(name: string) {
+  return `yield(name: "${name}")`;
 }
 
 function json2flux(json: FluxJsonQuery): string {
+  const result = [];
   const _bucket = from(json.bucket);
   const _range = range(json.range);
   const _filters = map(json.filters, filter);
-  return [_bucket, _range, ..._filters].join('\n  |> ');
+  result.push(_bucket, _range, ..._filters);
+
+  if (json.group) {
+    result.push(group(json.group));
+  }
+
+  if (json.aggregateWindow) {
+    result.push(aggregateWindow(json.aggregateWindow));
+  }
+
+  if (json.yield) {
+    result.push(fluxYield(json.yield))
+  }
+
+  return result.join('\n  |> ');
 }
 
 export default json2flux;
